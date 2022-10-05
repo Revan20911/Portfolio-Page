0e8c1c1b93ab5381b2dce27aecaacb4939c1eb54
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-10-05 15:45:32 -0500

    Update project-content.js

--- src/components/pages/project-content.js
+++ src/components/pages/project-content.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/projects.css";
 import anitracker from "../assets/Screenshot 2022-10-05 at 12-54-33 AniTracker.png";
+import weatherapp from "../assets/Screenshot 2022-10-05 at 15-22-44 Weather.png";
+import Link from "../ui/link";
 
 const Projects = () => {
     return(
@@ -9,29 +11,37 @@ const Projects = () => {
             <div className="project-content">
                 <div className="project-container">
                     <div className="image-container">
-                        <img alt=""  src={anitracker}/>
+                        <img  alt="" src={anitracker}/>
                     </div>
                     <div className="text-container">
                     <h1>AniTracker</h1>
-                    <p>777</p>
+                    <div className="row">
+                        <Link text="LIVE" link="https://anim-tracker.netlify.app/"/>
+                        <Link text="REPO" link="https://github.com/Revan20911/AniTracker"/>
                     </div>
-                    </div>
+                </div>
+                </div>
                 <div className="project-container-middle">
-                <div className="image-container"></div>
                 <div className="text-container">
-                <h1>AniTracker</h1>
-                <p>777</p>
+                <h1>Weather-App</h1>
+                <div className="row">
+                        <Link text="LIVE" link="https://revan20911.github.io/weather-app/"/>
+                        <Link text="REPO" link="https://github.com/Revan20911/weather-app"/>
+                    </div>
+                </div>
+                <div className="image-container">
+                    <img alt="" src={weatherapp}/>
                 </div>
                 </div>
                 <div className="project-container">
-                 
                 <div className="image-container"></div>
                 <div className="text-container">
                 <h1>AniTracker</h1>
-                <p>777</p>
+                <div className="row">
+                        <Link text="LIVE" link="https://anim-tracker.netlify.app/"/>
+                        <Link text="REPO" link="https://github.com/Revan20911/AniTracker"/>
+                    </div>
                 </div>
-               
-                
                 </div>
             </div>
         </div>
