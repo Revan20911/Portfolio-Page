9dfc5fe9c7300848f65feca0aec50dd638662939
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2022-10-05 14:30:30 -0500

    Update down_arrow_button.js

--- src/components/ui/down_arrow_button.js
+++ src/components/ui/down_arrow_button.js
@@ -2,7 +2,7 @@
 const ArrowDown = ({onClick, text, className}) =>{
     return(
         <>
-        <button className={className}onClick={onClick}><img/>
+        <button className={className}onClick={onClick}>
         </button>
         </>
     )
