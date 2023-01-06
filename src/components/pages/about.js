337ec9c275023d14734d452284be9fda1ad59cc5
chris <revan2091@live.com>
2023-01-06 12:08:41 -0600

    changed about section

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -8,7 +8,13 @@ const About = ({onClick}) => {
             <div className="content-container">
                 <div className="text-container">
                     <h1>About</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>
+
+I am a web developer with a passion for creating visually appealing and user-friendly websites. I have a strong foundation in HTML, CSS, and JavaScript, and I am constantly learning and experimenting with new technologies to enhance my skills. </p>
+
+<p>I have worked on a variety of projects, including building full-stack web applications, as well as creating responsive layouts for mobile and desktop devices. I am a problem-solver at heart and enjoy the challenge of finding creative solutions to complex problems.</p>
+
+<p>When I'm not coding, you can find me tinkering with new gadgets, playing the guitar, or working out at the gym. Thank you for visiting my portfolio, and I hope you enjoy exploring my work.</p>
                 </div>
             </div>
             <ArrowDown className={"down-arrow-2"} onClick={onClick}/>
