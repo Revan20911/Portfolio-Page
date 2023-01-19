e163f435cef6d778bdce829f65e0b495175ac834
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2023-01-19 17:30:29 -0600

    Update home.js

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,11 +1,12 @@
 
 
 const Home = () => {
+    const welcome = "Hi there, I'm Chris";    
     return(
         <div className="hero fixed  right-0 flex flex-row  min-h-screen  justify-center  bg-gray-400">
             <div className="hero-content text-left">
                 <div className="flex-col  max-w-md ">
-                    <h1 className="animate-typed text-5xl font-bold text-gray-700 w-0 whitespace-nowrap border-r-black border-r-2 overflow-hidden ">Hi there, I'm Chris</h1>
+                    <h1 className="animate-typed text-5xl font-bold text-gray-700 w-0 whitespace-nowrap  overflow-hidden ">{welcome}</h1>
                     <p className="opacity-0 mt-[100px] animate-slideUp py-6 text-gray-600 text-justify">I am a web developer with a passion for creating visually appealing and user-friendly websites. I have a strong foundation in HTML, CSS, and JavaScript, and I am constantly learning and experimenting with new technologies to enhance my skills.</p>
                 </div>
             </div>
