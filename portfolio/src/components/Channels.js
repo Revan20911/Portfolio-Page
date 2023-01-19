0eb1599238b435d791c43c55a1efd13d028d059d
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2023-01-19 17:35:41 -0600

    Update Channels.js

--- portfolio/src/components/Channels.js
+++ portfolio/src/components/Channels.js
@@ -20,8 +20,8 @@ function Channels({setChannel, sidebarStatus, setSideBarStatus}){
 
         function expandSideBar(){
 
-            const BarinitialClass = "fixed flex xs:top-0 xs:w-screen xs:h-0 lg:left-0 lg:w-0 lg:h-screen flex-col bg-gray-800 h-screen  transition-all 1s ease-in-out opacity-0 z-0";
-            const HeaderInitialClass =  " flex fixed xs:w-screen lg:w-80 lg:h-0 bg-gray-800 border-b border-gray-800 shadow-md items-center justify-center transition-all 1s opacity-0"
+            const BarinitialClass = "fixed  xs:top-0 xs:w-screen xs:h-0 lg:left-0 lg:w-0 lg:h-screen none bg-gray-800 h-screen  transition-all 1s ease-in-out opacity-0 z-0";
+            const HeaderInitialClass =  " none fixed xs:w-screen lg:w-80 lg:h-0 bg-gray-800 border-b border-gray-800 shadow-md items-center justify-center transition-all 1s opacity-0"
 
             if(sidebarStatus === 'active'){
                 ChannelBar.current.className = "fixed flex lg:left-16 lg:top-0 flex-col xs:h-80 xs:pr-6 xs:top-14 xs:left-0 xs:w-screen  bg-gray-800 lg:h-screen lg:w-80  transition-all 1s ease-in-out opacity-100 z-10";
