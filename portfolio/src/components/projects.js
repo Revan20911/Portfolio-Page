77f653997184f2c0a49e3e7c2d0e7ec0b7d76293
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2023-02-22 15:24:32 -0600

    Update projects.js

--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -29,13 +29,13 @@ const Projects = () => {
     }
 
     return(
-        <div className="flex flex-col  py-10 px-10 justify-center w-fit h-screen ">
-            <div className="grid xs:grid-cols-1 lg:grid-cols-auto lg:grid-rows-auto sm:gap-y-6 xs:w-screen  mx-0 my-0 md:grid-cols-3 md:gap-x-48 md:gap-y-0 lg:gap-x-48 lg:gap-y-0 sm:row-span-10 md:grid-rows-auto sm:grid-cols-auto sm:grid-rows-auto  sm:w-screen lg:w-fit w-full h-screen overflow-scroll scrollbar-hide
+        <div className="flex flex-col pb-10 px-10 w-fit h-screen  ">
+            <div className="grid px-56 pb-24 xs:grid-cols-1 lg:grid-cols-2 lg:grid-rows-auto sm:gap-y-6 xs:w-screen xl:grid-cols-3  md:grid-cols-3 md:gap-x-48 md:gap-y-0 lg:gap-x-24 lg:gap-y-24 sm:row-span-10 md:grid-rows-auto sm:grid-cols-auto sm:grid-rows-auto  sm:w-screen lg:w-fit w-full h-screen overflow-scroll scrollbar-hide
             transition-all 1s ease-in-out">
                 {
                     projects.map((project, index) => {
-                        return <div  key={index} className="card xs:w-96 h-fit sm:w-[36rem]  lg:w-80 bg-gray-500 shadow-xl md:mr-2 mb-0 lg:mr-2">
-                            <figure ><img className="min-w-fit max-w-full min-h-md max-h-48"src={project.img_url || <Skeleton/>} alt=""/></figure>
+                        return <div  key={index} className="xs:w-96 h-fit w-96 sm:w-[36rem]   bg-gray-500 shadow-xl md:mr-2 mb-0 lg:mr-2">
+                            <figure className="w-full h-72" ><img className="h-72 w-full" src={project.img_url || <Skeleton/>} alt=""/></figure>
                             <div className="card-body ">
                                 <h2 className="card-title text-gray-100 text-3xl font-extrabold">{project.name || <Skeleton/>}</h2>
                                 <div className="flex h-38 flex-wrap ">
