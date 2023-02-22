aaf339e0224a287fb12c2e6cc9f3e78d9e7bc5b1
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2023-02-22 15:52:13 -0600

    Update skills.js

--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -15,7 +15,7 @@ const Skills = () => {
 
 
     return(
-        <div className="grid grid-cols-5 grid-flow-row h-fit w-fit mx-5 my-5">
+        <div className="grid grid-cols-6  h-full w-full mx-5 my-5 px-96 py-48 gap-y-3 gap-x-14 ">
             {GetSkills(languages, onClick)}
             {GetSkills(frameworks, onClick)}
             {GetSkills(databases, onClick)}
@@ -33,15 +33,16 @@ function GetSkills(valueList, onClick){
     
 
     return valueList.map((item, index)=>{
-        return <div key={index} onClick={onClick}  className="  flex cursor-pointer flex-row justify-center relative h-48 w-48 rounded-box mx-3 mt-3 mb-3 overflow-hidden hover:overflow-visible"  >
+        return <div key={index} onClick={onClick}  className="flex cursor-pointer flex-col justify-center items-center h-56 w-72 p-5  bg-gray-600 "  >
+
+              
+        <img className=" h-24 w-24 mb-4  "src={item.img_url} alt=""/>
+        <h1 className="text-gray-300 font-bold text-2xl">{item.name}</h1>
+        <p className="text-gray-300 font-semibold text-md"></p>
         
-        <img className="fixed shadow-xl rounded-box h-48 w-48 "src={item.img_url} alt=""/>
-        <div 
-        className=" flex hover:backdrop-blur-2xl flex-col text-2xl font-bold items-center text-opacity-0 justify-center  w-full h-full bg-opacity-0  bg-gray-500 hover:bg-opacity-70 hover:text-opacity-100  absolute top-0 bottom-0 left-0 right-0 rounded-3xl transition-all 1s ease-in-out text-gray-100">
-            {item.name}
-            </div>
+    
             
-        
+       
         </div>
     })  
 }
